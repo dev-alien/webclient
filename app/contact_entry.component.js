75cdb02e77d51dd38a9ad3c39a6e768a73956ca6
jmpeax <jmpeax@icloud.com>
2017-07-10 22:00:30 +0300

    before update

--- app/contact_entry.component.js
+++ app/contact_entry.component.js
@@ -39,16 +39,24 @@ System.register(["@angular/core", "./session.service", "./window_mgr.service", "
                     this.sWindowManager = sWindowManager;
                     this.sApiClient = sApiClient;
                     this.sAlienCrypto = sAlienCrypto;
+                    this.onContactDeleted = new core_1.EventEmitter();
                 }
+                ContactEntryComponent.prototype.deleteContact = function (user) {
+                    this.onContactDeleted.emit(this.contact);
+                };
                 __decorate([
                     core_1.Input(), 
                     __metadata('design:type', user_model_1.User)
                 ], ContactEntryComponent.prototype, "contact", void 0);
+                __decorate([
+                    core_1.Output(), 
+                    __metadata('design:type', core_1.EventEmitter)
+                ], ContactEntryComponent.prototype, "onContactDeleted", void 0);
                 ContactEntryComponent = __decorate([
                     core_1.Component({
                         selector: 'app-contact-entry',
-                        template: "\n        {{ contact.login }}\n    ",
-                        styles: ["\n            \n    "]
+                        template: "\n        <div class=\"contacts_entry\">\n            <div class=\"remove_icon\" (click)=\"deleteContact(entry)\"></div>\n            <div class=\"contacts_icon\">\n                <div class=\"online_status green\">\n                </div>\n            </div>\n            <div class=\"contacts_content\">\n                <div class=\"contact_name\">\n                        <span class=\"contact_first_name\" *ngIf=\"contact.info.first_name\" >\n                            {{ contact.info.first_name }}\n                        </span>\n                    <span class=\"contact_last_names\" *ngIf=\"contact.info.first_name\" >\n                            {{ contact.info.last_name }}\n                        </span>\n                    @{{ contact.login }}\n                </div>\n                <div class=\"last_\">\n\n                </div>\n            </div>\n        </div>\n    ",
+                        styles: ["\n        .contacts_icon {\n            position: absolute;\n            margin-left: 5px;\n            width: 32px;\n            height: 32px;\n            border: 2px solid black;\n        }\n\n        .contact_name {\n            padding-top: 9px;\n            padding-left: 50px;\n            font-weight: bold;\n        }\n\n        .contacts_entry {\n            background-color: white;\n            margin-top: 5px;\n            width: 300px;\n            height: 40px;\n            padding-top: 5px;\n        }\n\n        .contacts_entry:hover {\n            background-color: lightgrey;\n        }\n    "]
                     }), 
                     __metadata('design:paramtypes', [session_service_1.UserSession, window_mgr_service_1.WindowManager, api_client_service_1.ApiClient, alien_crypto_service_1.AlienCrypto])
                 ], ContactEntryComponent);
--- app/contact_entry.component.ts
+++ app/contact_entry.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit, ElementRef, Input, OnChanges, SimpleChange, AfterViewChecked} from "@angular/core";
+import {
+    Component, OnInit, ElementRef, Input, OnChanges, SimpleChange, AfterViewChecked, Output,
+    EventEmitter
+} from "@angular/core";
 import {UserSession} from "./session.service";
 import {WindowManager} from "./window_mgr.service";
 import {ApiClient} from "./api_client.service";
@@ -11,17 +14,66 @@ declare var jQuery:any;
 @Component({
     selector: 'app-contact-entry',
     template: `
-        {{ contact.login }}
+        <div class="contacts_entry">
+            <div class="remove_icon" (click)="deleteContact(entry)"></div>
+            <div class="contacts_icon">
+                <div class="online_status green">
+                </div>
+            </div>
+            <div class="contacts_content">
+                <div class="contact_name">
+                        <span class="contact_first_name" *ngIf="contact.info.first_name" >
+                            {{ contact.info.first_name }}
+                        </span>
+                    <span class="contact_last_names" *ngIf="contact.info.first_name" >
+                            {{ contact.info.last_name }}
+                        </span>
+                    @{{ contact.login }}
+                </div>
+                <div class="last_">
+
+                </div>
+            </div>
+        </div>
     `,
     styles: [`
-            
+        .contacts_icon {
+            position: absolute;
+            margin-left: 5px;
+            width: 32px;
+            height: 32px;
+            border: 2px solid black;
+        }
+
+        .contact_name {
+            padding-top: 9px;
+            padding-left: 50px;
+            font-weight: bold;
+        }
+
+        .contacts_entry {
+            background-color: white;
+            margin-top: 5px;
+            width: 300px;
+            height: 40px;
+            padding-top: 5px;
+        }
+
+        .contacts_entry:hover {
+            background-color: lightgrey;
+        }
     `]
 })
 export class ContactEntryComponent {
     @Input() contact: User;
+    @Output() onContactDeleted: EventEmitter<User> = new EventEmitter<User>();
     constructor(public sSessionManager: UserSession,
                 public sWindowManager: WindowManager,
                 public sApiClient: ApiClient,
                 public sAlienCrypto: AlienCrypto) {
+    }
+
+    public deleteContact(user: User) {
+        this.onContactDeleted.emit(this.contact);
     }
 }
